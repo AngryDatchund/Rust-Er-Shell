248874ee1b7a2dd4dd43e1d5121ba225225370bf
Havier <admin@dackelstudios.com>
2023-12-02 14:27:15 +0300

    Removing files
    Removing files and intergrating them into script

--- Resources/Example.bat
+++
@@ -1,6 +0,0 @@
-cd RustServer
-
-:start
-RustDedicated.exe -batchmode -nographics -logFile Log.txt +server.identity "example" +oxide.directory "server/example/oxide/"
-timeout /t 10 /nobreak
-goto start
